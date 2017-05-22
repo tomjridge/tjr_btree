1d0018b001007f01075cf8af49b584e13503a84e
Tom Ridge <tom.j.ridge@googlemail.com>
2017-05-22 11:54:31 +0100

    +

--- src/bash_env.sh
+++ src/bash_env.sh
@@ -7,13 +7,12 @@ src_subdirs=`echo {ad,ag,b,c,d,e,f,g,h,i,j,n}_*`
 mls_in_subdirs=`ls {ad,ag,b,c,d,e,f,g,h,i,j,n}_*/*.ml`
 meta_description="A CoW B-tree library"
 
-PKGS1="num,yojson,ppx_deriving_yojson,batteries,extunix,extlib"
-PKGS="-package num,yojson,ppx_deriving_yojson,batteries,extunix,extlib"
+required_packages="num,yojson,ppx_deriving_yojson,batteries,extunix,extlib"
+
+natives="main.native test_main.native simple_example.native"
+bytes="test_main.byte"
 
 source bash_env.common
 
-natives="main.native test_main.native simple_example.native"
-
-bytes="test_main.byte"
 
 
