625c228746c7088c993bd242d708862280602260
Tom Ridge <tom.j.ridge@googlemail.com>
2017-06-02 16:08:17 +0100

    add an int->int interface to main

--- src/i_examples/int_int_map_on_fd.ml
+++ src/i_examples/int_int_map_on_fd.ml
@@ -25,3 +25,38 @@ let map_ops =
   Store_to_map.store_ops_to_map_ops ~ps ~page_ref_ops ~store_ops
 
 let ls_ops = mk_ls_ops ~ps ~page_ref_ops ~store_ops
+
+
+let from_file ~fn ~create ~init = from_file ~fn ~create ~init ~ps
+
+let main args = (
+  (* turn off wf checking *)
+  Test.disable ();
+  match args with
+  | ["init"; fn] -> (
+      from_file ~fn ~create:true ~init:true
+      |> (fun _ -> print_endline "init ok"))
+  | ["insert";fn;k;v] -> (
+      from_file ~fn ~create:false ~init:false
+      |> map_ops.insert (int_of_string k) (int_of_string v) 
+      |> function (t,Ok _) -> (close ~blk_sz t))
+  | ["delete";fn;k] -> (
+      from_file ~fn ~create:false ~init:false
+      |> map_ops.delete (int_of_string k) 
+      |> function (t,Ok _) -> (close ~blk_sz t))
+  | ["list";fn] -> (
+      from_file ~fn  ~create:false ~init:false
+      |> (fun s -> 
+          s 
+          |> all_kvs ls_ops 
+          |> (function (s',Ok kvs) -> (
+                (List.iter (fun (k,v) -> 
+                     Printf.printf "%s -> %s\n" (string_of_int k) 
+                       (string_of_int v)) kvs);
+                close ~blk_sz s';
+                ()));                
+          print_endline "list ok"))
+  | _ -> (
+      failwith ("Unrecognized args: "^(String_.concat_strings " " args)^ 
+                __LOC__))
+)
--- src/m_main/main.ml
+++ src/m_main/main.ml
@@ -5,6 +5,7 @@ let args = Sys.argv |> Array.to_list |> List.tl
 let _ = 
   match args with
   | "kv"::rest -> (Ss_ss_map_on_fd.main rest)
+  | "ii"::rest -> (Int_int_map_on_fd.main rest)
   | _ -> failwith ("Unrecognized arguments: "^
                    (String_.concat_strings " " args)^
                    __LOC__)
