526d58933ed2d59e5beb679af7ae93a1650aac6c
Tom Ridge <tom.j.ridge@googlemail.com>
2017-09-06 11:30:44 +0100

    fixup doc

--- src/n_doc/tjr_btree_doc.ml
+++ src/n_doc/tjr_btree_doc.ml
@@ -37,102 +37,114 @@ Exported code is in the [from_isa] package. There is some patching of
    certainly isn't readable.
 
 
-{2 Pickle}
+   {2 Test}
 
-The marshalling and pickling routines are in the [pickle] package. See
-   module {!Pickle}. We also include a utility library for strings
-   {!Tjr_string}, and a few functions to deal with logging and testing
-   in {!Test}.
+   Some simple testing utility functions.
 
 
-{2 Base types}
+   {2 Base types}
 
-This package contains basic B-tree related types. They are collected
-   in the module {!Base_types}. The {!Base_types.Monad} is a
-   state-passing monad with error; this monad shows up in most of the
-   interfaces. {!Store_ops} is another important module which
-   describes a disk-like interface on top of which we implement the
-   B-tree. The {!Base_types.Tree} module describes a B-tree as an
-   algebraic datatype. The on-disk B-tree uses references between
-   blocks i.e. a graph-like structure with pointers rather than a
-   datatype. Indeed, the tree datatype is used only for testing
-   purposes.
+   This package contains basic B-tree related types:
+
+- {!Block} for blocks on disk
+- {!Constants} which give size constraints on B-tree nodes
+- {!Frame} which imports the Isabelle frame type
+- {!Ls_state} for the leaf stream state
+- {!Monad} for the state and error monad
+- {!R2f} and {!R2t} for internal types used in testing
+- {!Rstk} for the type of framestacks
+- {!Tree} for the tree type imported from Isabelle
+
+   The {!Base_types.Monad} is a state-passing monad with error; this
+   monad shows up in most of the interfaces.
+
+   The {!Base_types.Tree} module describes a B-tree as an algebraic
+   datatype. The on-disk B-tree uses references between blocks i.e. a
+   graph-like structure with pointers rather than a datatype. Indeed,
+   the tree datatype is used only for testing purposes.
 
 
+   {2 Api}
 
-{2 Prelude}
+   This package gives the main interface types, including interfaces
+   for disk, store and map (via types such as [`Map_ops]).
 
-Miscellaneous definitions. {!Prelude.Small_step} wraps the Isabelle
-   small-step definitions.
+   - {!Disk_ops} for low-level block interface
+   - {!Store_ops} for a layer just above {!Disk_ops}
+   - {!Leaf_stream_ops} for leaf streams
+   - {!Map_ops} for map operations, find etc.
+   - {!Page_ref_int} utility to fix page ref as an integer
+   - {!Pre_map_ops} like {!Map_ops} but with explicit state passing
 
-
-{2 Api}
-
-{!Btree_api} gives the main interface types, including interfaces for
-   disk, store and map (via record types such as [map_ops]). In
-   addition, the leaf stream interface allows to iterate over the
+   In addition, the leaf stream interface allows to iterate over the
    leaves in a B-tree e.g. to find all the bindings in the map. This
-   module also documents the type variable naming conventions.
+   module also documents the type variable naming conventions (see
+   {!Map_ops}).
 
-{!Default} is a simple collection of defaults eg default block size.
 
+   {2 Store to map}
 
-{2 Store to map}
-
-This package, particularly the {!Store_to_map} module, wraps the
+   This package, particularly the {!Store_to_map} module, wraps the
    Isabelle routines to implement a map interface on top of a
-   store. The key function is {!Store_to_map.make_map_ops} which takes
-   a [store_ops] and returns a [map_ops].
+   store. The key function is {!Store_to_map.store_ops_to_map_ops}
+   which takes a [store_ops] and returns a [map_ops].
 
 
+   {2 Disks}
 
-{2 Disks}
+   {!Disk_on_fd} is a persistent disk on top of a file. 
 
-{!Disk_on_fd} is a persistent disk on top of a file. {!Disk_to_store}
-   includes a function that naively transforms a disk to a store. This
-   involves converting a {!Frame} to a block, using
-   {!Btree_with_pickle} TODO rename.
-
-
-{2 Stores}
-
-{!Mem_store} is an in-memory store. {!Recycling_store} is a store that
-   optimizes page alloc and free to avoid too many unnecessary writes.
+   {!Disk_to_store} includes a function that naively transforms a disk
+   to a store. 
 
 
-{2 Pre examples}
+   {2 Stores}
 
-This package contains various modules needed by the examples, such as
-   {!Small_string} which implements strings upto 256 bytes (used for
-   keys in the B-tree).
+   {!Mem_store} is an in-memory store. 
 
-
-{2 Cache}
-
-A generic LRU {!Cache} on top of a map.
+   {!Recycling_store} is a store that optimizes page alloc and free to
+   avoid too many unnecessary writes.
 
 
-{2 Examples}
+   {2 Binprot marshalling}
 
-Various examples. {!Ss_ss_map_on_fd} implements an on-disk map from
-   (small) string to string. {!Int_int_map_on_fd} and
-   {!Ss_int_map_on_fd} are similar. {!Map_on_fd} is the generic
-   version and {!Mem_map} is a map on top of the
-   {!Mem_store}. {!Bytestore} is TODO and should implement arbitrary
-   long values.
+   {!Binprot_marshalling} provides on-disk marshalling courtesy of
+   [binprot]. Or roll your own.
 
 
-{2 Testing}
+   {2 Cache}
 
-Various tests.
+   A generic LRU {!Cache} on top of a map.
 
 
-{2 Main}
+   {2 Examples}
 
-These [xxx.ml] files get turned into executables
-   [xxx.native]. [main.native] is used by the [kv_main.sh] example;
-   [test_main.native] is used for testing; [simple_example.native] is
-   the simple example.
+   Various examples. 
+
+   {!Ss_ss_map_on_fd} implements an on-disk map from (small) string to
+   string.
+
+   {!Map_int_int} and {!Ss_int_map_on_fd} are similar. {!Map_on_fd} is
+   the generic version and {!Mem_map} is a map on top of the
+   {!Mem_store}. 
+
+   {!Bytestore} is TODO and should implement arbitrary long values.
+
+
+   {2 Testing}
+
+   Various tests.
+
+
+   {2 Main}
+
+   These [xxx.ml] files get turned into executables
+   [xxx.native]. 
+   
+   - [ii_example.native] is the [int -> int] example
+   - [main.native] is used by the [kv_main.sh] example
+   - [test_main.native] is used for testing
+   - [simple_example.native] is the [string->string] kv example
 
 *)
 let dummy = 1
