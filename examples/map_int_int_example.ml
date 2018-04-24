bce025bc8960b7a7ca4ccb10d3bb28a1d4fe7fcf
Tom Ridge <tom.j.ridge@googlemail.com>
2018-04-24 12:31:41 +0100

    wip separating examples, test and src

---
+++ examples/map_int_int_example.ml
@@ -0,0 +1,39 @@
+(* a map from int to int, backed by file ------------------------------- *)
+
+open Map_int_int
+
+
+let main args = (
+  (* turn off wf checking *)
+  Test.disable ();
+  Map_ops.dest_map_ops map_ops @@ fun ~find ~insert ~delete ~insert_many ->
+  match args with
+  | ["init"; fn] -> (
+      from_file ~fn ~create:true ~init:true |> fun _ -> 
+      print_endline "init ok")
+  | ["insert";fn;k;v] -> (
+      from_file ~fn ~create:false ~init:false |> fun s ->
+      insert (int_of_string k) (int_of_string v)  
+      |> run s 
+      |> function (t,_) -> close t)
+  | ["delete";fn;k] -> (
+      from_file ~fn ~create:false ~init:false |> fun s ->
+      delete (int_of_string k) 
+      |> run s 
+      |> function (t, _) -> close t)
+  | ["list";fn] -> (
+      from_file ~fn  ~create:false ~init:false |> fun s -> 
+      Leaf_stream_util.all_kvs ~ls_ops 
+      |> run s 
+      |> (function (s',kvs) -> (
+            (List.iter (fun (k,v) -> 
+                 Printf.printf "%s -> %s\n" (string_of_int k) 
+                   (string_of_int v)) kvs);
+            close s';
+            ()));                
+      print_endline "list ok")
+  | _ -> (
+      failwith (
+        "Unrecognized args: "^
+        (String_.concat_strings " " args)^ 
+        __LOC__)) )
--- src/j_test/default_filename.ml
+++ src/j_test/default_filename.ml
@@ -1,2 +1,3 @@
 (** Default filename for testing. *)
+(* FIXME isn't this a bit horrible? put in a config file? *)
 let fn = "./btree.store"
